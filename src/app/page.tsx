5ee2f1eda8fdbe4ba13c5abc713ea8b4edf59432
Gustavo Francisco <ciscosweater@gmail.com>
2025-02-24 17:20:50 -0300

    lógica e página do portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,55 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import AboutMe from "./pages/about/about";
+import React from "react";
+import Experiences from "./pages/experiences/experiences";
+import Technologies from "./pages/technologies/technologies";
+import Projects from "./pages/projects/projects";
+
+const OPTIONS: any = {
+  about: { component: AboutMe, label: 'Sobre mim' },
+  tech: { component: Technologies, label: 'Tecnologias' },
+  exp: { component: Experiences, label: 'Experiência' },
+  projects: { component: Projects, label: 'Projetos' }
+};
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [option, setOption] = React.useState("about");
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const RenderedComponent = React.useMemo(() => {
+    const Component = OPTIONS[option].component;
+    return <Component />;
+  }, [option]);
+
+  const renderMenuOption = (key: any) => (
+    <React.Fragment key={key}>
+      <h3
+        className={option === key ? styles.optionSelected : styles.option}
+        onClick={() => setOption(key)}
+      >
+        {OPTIONS[key].label}
+      </h3>
+      {key !== 'projects' && <div className={styles.barDivider} />}
+    </React.Fragment>
+  );
+
+  return (
+    <>
+      <div className={styles.mainPage}>
+        <div className={styles.center}>
+          <h1>Oi, eu sou Gustavo Francisco!</h1>
+          <h2>Desenvolvedor FullStack</h2>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className={styles.bottom}>
+          <h4>Role pra baixo para saber mais</h4>
+        </div>
+      </div>
+      <div className={styles.selectionBar}>
+        {Object.keys(OPTIONS).map(renderMenuOption)}
+      </div>
+      {RenderedComponent}
+    </>
   );
 }
---
+++ src/app/pages/about/about.tsx
@@ -0,0 +1,9 @@
+import styles from "./about.module.css";
+
+export default function AboutMe() {
+    return (
+        <div className={styles.page}>
+            <h2>Sobre mim</h2>
+        </div>
+    )
+}
